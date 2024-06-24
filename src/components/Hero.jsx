41d419efe4a5fc55edc74588fca046a2f64d80b9
JackHarperIMF <hemvayyala8@gmail.com>
2024-06-24 15:53:46 +0530

    responsivness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,10 +17,10 @@ const Hero = () => {
     gsap.to('#cta', {opacity:1, delay: 1.5, duration:1, y: -50})
   }, [])
 
-  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 720 ? smallHeroVideo : heroVideo)
 
   const handleVideoSrcSet = () => {
-    if(window.innerWidth < 760){
+    if(window.innerWidth < 720){
       setVideoSrc(smallHeroVideo)
     } else {
       setVideoSrc(heroVideo)
@@ -35,23 +35,23 @@ const Hero = () => {
     }
   })
 
-  return <>
-      <section className="w-full nav-height bg-black relative sm:my-20">
-        <div className="h-5/6 w-full flex-center flex-col">
-          <p id="hero" className="hero-title">iPhone 15 Pro</p>
-          <div className="md:w-10/12 w-9/12">
-            <video className="pointer-events-none" autoPlay muted playsInline key={videoSrc}>
-              <source src={videoSrc} type="video/mp4"/>
-            </video>
-          </div>
+  return (
+    <section className="flex-col flex-center w-full nav-height bg-black relative">
+      <div className="w-full flex-col flex-center mt-10">
+        <p id="hero" className="hero-title">iPhone 15 Pro</p>
+        <div className="md:w-10/12 w-9/12">
+          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4"/>
+          </video>
         </div>
+      </div>
 
-        <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20 my-20">
-          <a href="#highlights" className="btn">Buy</a>
-          <p className="font-normal text-xl">From $199/month or $999</p>
-        </div>
-      </section>
-    </>
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { appleImg, bagImg, searchImg } from '../utils'
 
 const Navbar = () => {
   return <>
-    <header className='w-full py-3 px-5 flex justify-center items-center sm:px-10 '>
-      <nav className='flex justify-center w-full screen-max-width'>
+    <header className='w-full py-3 px-5 flex justify-center items-center sm:px-10'>
+      <nav className='flex justify-center w-full screen-max-width max-md:justify-between'>
         <img src={appleImg} alt="Apple" width={14} height={18}/>
 
-        <div className='flex mx-7 justify-center max-sm:hidden max-md:flex-1'>  
+        <div className='flex mx-7 justify-center max-md:hidden'>  
           {navLists.map((nav) => (
             <div className='px-4 text-sm cursor-pointer text-gray hover:text-white transition-all' key={nav}>
               {nav}
